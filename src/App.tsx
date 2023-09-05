b2af7124fcb7fe291b1a2a893a9028fea58ffd24
Alexandre Carvalho <alex380bb@gmail.com>
2023-09-04 21:17:13 -0300

    req 2 e 3

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,28 @@ import './App.css';
 import Book from './components/Book';
 import BookList from './components/BookList';
 import data from './data.json';
-
-const bookIndexStart = 10;
-const bookIndexEnd = 15;
-const placeHolderList = data.slice(bookIndexStart, bookIndexEnd); // esse código deverá ser excluído após a implementação do requisito 2
+import { BookListType } from './types';
 
 function App() {
-
   const [livroAtual, setLivroAtual] = useState(0);
+  const [wishList, setWishList] = useState<BookListType>([]);
+  const [readingList, setReadingList] = useState<BookListType>([]);
+  const [readList, setReadList] = useState<BookListType>([]);
 
   return (
     <div className="app">
       <div className="book-selector">
-      <Book bookInfo={ data[livroAtual] } showDetails />
+        <Book bookInfo={ data[livroAtual] } showDetails />
         <div className="selector-buttons">
-          <button>Adicionar à lista de desejos</button>
+          <button onClick={ () => setWishList([...wishList, data[livroAtual]]) }>
+            Adicionar à lista de desejos
+          </button>
+          <button onClick={ () => setReadingList([...readingList, data[livroAtual]]) }>
+            Adicionar à lista de leitura
+          </button>
+          <button onClick={ () => setReadList([...readList, data[livroAtual]]) }>
+            Adicionar à lista de lidos
+          </button>
           <button>Adicionar à lista de leitura</button>
           <button>Adicionar à lista de lidos</button>
           <button onClick={ () => setLivroAtual((livroAtual + 1) % data.length) }>
@@ -31,8 +38,8 @@ function App() {
         <button>Exibir lista de leitura</button>
         <button>Exibir lista de lidos</button>
       </div>
-      <h1>Lista de ...</h1>
-      <BookList books={ placeHolderList } />
+      <h1>Lista de desejos</h1>
+      <BookList books={ wishList } />
     </div>
   );
 }
